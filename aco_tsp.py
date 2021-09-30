eb873d7aa769e1c1367106fbe3361a5a8599804e
Himanshu Kaushik <himanshu.kaushik@delhivery.com>
2021-09-30 12:57:19 +0530

    added method to solve for pickup and delivery conditions

--- aco_tsp.py
+++ aco_tsp.py
@@ -1,53 +1,77 @@
 import math
 import random
+import numpy as np
 from matplotlib import pyplot as plt
 
 
+VERY_LARGE_NUMBER = 10000
+
+def distance(a, b):
+    """Calculates distance between two latitude-longitude coordinates."""
+    R = 3963  # radius of Earth (miles)
+    lat1, lon1 = math.radians(a[0]), math.radians(a[1])
+    lat2, lon2 = math.radians(b[0]), math.radians(b[1])
+    return math.acos(math.sin(lat1) * math.sin(lat2) +
+                     math.cos(lat1) * math.cos(lat2) * math.cos(lon1 - lon2)) * R
+
+class Edge:
+    def __init__(self, a, b, weight, initial_pheromone):
+        self.a = a
+        self.b = b
+        self.weight = weight
+        self.pheromone = initial_pheromone
+
+class Ant:
+    def __init__(self, alpha, beta, num_nodes, edges):
+        self.alpha = alpha
+        self.beta = beta
+        self.num_nodes = num_nodes
+        self.edges = edges
+        self.tour = None
+        self.distance = 0.0
+
+    def _select_node(self):
+        roulette_wheel = 0.0
+        unvisited_nodes = self.available_nodes#[node for node in range(self.num_nodes) if node not in self.tour]
+        heuristic_total = 0.0
+        for unvisited_node in unvisited_nodes:
+            heuristic_total += self.edges[self.tour[-1]][unvisited_node].weight
+        for unvisited_node in unvisited_nodes:
+            roulette_wheel += pow(self.edges[self.tour[-1]][unvisited_node].pheromone, self.alpha) * \
+                              pow((heuristic_total / self.edges[self.tour[-1]][unvisited_node].weight), self.beta) if \
+                self.edges[self.tour[-1]][unvisited_node].weight else VERY_LARGE_NUMBER
+        random_value = random.uniform(0.0, roulette_wheel)
+        wheel_position = 0.0
+        for unvisited_node in unvisited_nodes:
+            wheel_position += pow(self.edges[self.tour[-1]][unvisited_node].pheromone, self.alpha) * \
+                              pow((heuristic_total / self.edges[self.tour[-1]][unvisited_node].weight), self.beta) if \
+                self.edges[self.tour[-1]][unvisited_node].weight else VERY_LARGE_NUMBER
+            if wheel_position >= random_value:
+                return unvisited_node
+
+    def update_available_nodes(self):
+        for node in set(self.tour).intersection(self.available_nodes):
+            self.available_nodes.remove(node)
+            if node < self.num_nodes / 2:
+                self.available_nodes.append(int(node + self.num_nodes/2))
+
+    def find_tour(self):
+        self.available_nodes = list(range(int(self.num_nodes / 2)))
+        self.tour = list(np.random.choice(self.available_nodes, 1))
+        self.update_available_nodes()
+        while len(self.tour) < self.num_nodes:
+            self.tour.append(self._select_node())
+            self.update_available_nodes()
+        return self.tour
+
+    def get_distance(self):
+        self.distance = 0.0
+        for i in range(self.num_nodes):
+            self.distance += self.edges[self.tour[i]][self.tour[(i + 1) % self.num_nodes]].weight
+        return self.distance
+
+
 class SolveTSPUsingACO:
-    class Edge:
-        def __init__(self, a, b, weight, initial_pheromone):
-            self.a = a
-            self.b = b
-            self.weight = weight
-            self.pheromone = initial_pheromone
-
-    class Ant:
-        def __init__(self, alpha, beta, num_nodes, edges):
-            self.alpha = alpha
-            self.beta = beta
-            self.num_nodes = num_nodes
-            self.edges = edges
-            self.tour = None
-            self.distance = 0.0
-
-        def _select_node(self):
-            roulette_wheel = 0.0
-            unvisited_nodes = [node for node in range(self.num_nodes) if node not in self.tour]
-            heuristic_total = 0.0
-            for unvisited_node in unvisited_nodes:
-                heuristic_total += self.edges[self.tour[-1]][unvisited_node].weight
-            for unvisited_node in unvisited_nodes:
-                roulette_wheel += pow(self.edges[self.tour[-1]][unvisited_node].pheromone, self.alpha) * \
-                                  pow((heuristic_total / self.edges[self.tour[-1]][unvisited_node].weight), self.beta)
-            random_value = random.uniform(0.0, roulette_wheel)
-            wheel_position = 0.0
-            for unvisited_node in unvisited_nodes:
-                wheel_position += pow(self.edges[self.tour[-1]][unvisited_node].pheromone, self.alpha) * \
-                                  pow((heuristic_total / self.edges[self.tour[-1]][unvisited_node].weight), self.beta)
-                if wheel_position >= random_value:
-                    return unvisited_node
-
-        def find_tour(self):
-            self.tour = [random.randint(0, self.num_nodes - 1)]
-            while len(self.tour) < self.num_nodes:
-                self.tour.append(self._select_node())
-            return self.tour
-
-        def get_distance(self):
-            self.distance = 0.0
-            for i in range(self.num_nodes):
-                self.distance += self.edges[self.tour[i]][self.tour[(i + 1) % self.num_nodes]].weight
-            return self.distance
 
     def __init__(self, mode='ACS', colony_size=10, elitist_weight=1.0, min_scaling_factor=0.001, alpha=1.0, beta=3.0,
                  rho=0.1, pheromone_deposit_weight=1.0, initial_pheromone=1.0, steps=100, nodes=None, labels=None):
@@ -67,10 +91,9 @@ class SolveTSPUsingACO:
         self.edges = [[None] * self.num_nodes for _ in range(self.num_nodes)]
         for i in range(self.num_nodes):
             for j in range(i + 1, self.num_nodes):
-                self.edges[i][j] = self.edges[j][i] = self.Edge(i, j, math.sqrt(
-                    pow(self.nodes[i][0] - self.nodes[j][0], 2.0) + pow(self.nodes[i][1] - self.nodes[j][1], 2.0)),
+                self.edges[i][j] = self.edges[j][i] = Edge(i, j, distance(self.nodes[i],self.nodes[j]),
                                                                 initial_pheromone)
-        self.ants = [self.Ant(alpha, beta, self.num_nodes, self.edges) for _ in range(self.colony_size)]
+        self.ants = [Ant(alpha, beta, self.num_nodes, self.edges) for _ in range(self.colony_size)]
         self.global_best_tour = None
         self.global_best_distance = float("inf")
 
